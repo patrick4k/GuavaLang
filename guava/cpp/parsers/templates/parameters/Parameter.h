a6c9be5f2bbd7439f4e9b07ca6e218c0fa4610ad
patrick4k <pmkennedy4@gmail.com>
2023-10-09 11:11:19 -0400

    Missing Parameter.h

---
+++ guava/cpp/parsers/templates/parameters/Parameter.h
@@ -0,0 +1,31 @@
+//
+// Created by Patrick on 10/6/2023.
+//
+
+#ifndef GUAVA_PARAMETER_H
+#define GUAVA_PARAMETER_H
+
+#include <memory>
+#include "../../../guava-types.h"
+#include "../IType.h"
+#include "../expressions/IExpression.h"
+
+using namespace guavalang;
+
+namespace guavaparser {
+
+    class Parameter {
+    public:
+        explicit Parameter(Ptr<IExpression> id, Ptr<IExpression> type) :
+        m_id(std::move(id)),
+        m_type(std::move(type))
+        {
+        }
+
+    private:
+        Ptr<IExpression> m_id{};
+        Ptr<IExpression> m_type{};
+    };
+}
+
+#endif //GUAVA_PARAMETER_H
